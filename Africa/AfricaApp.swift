1da3396ab00a77948296cccabd0624770b121063
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-27 23:16:44 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
