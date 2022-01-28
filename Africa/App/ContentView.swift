2514ccb2205d5ec90accf5e493f2f467397e2080
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-28 00:56:20 -0500

    Adding the List of animals, with the model, data and i modified the extension file for to decode any file of JSON.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     //MARK: - PROPERTIES
     
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     //MARK: - BODY
     
     var body: some View {
@@ -20,6 +22,12 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
+                .listRowBackground(Color.black)
+                .padding(.top, 20)
             } //: LIST
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [CoverImage] {
+    func decode<T: Codable>(_ file: String) -> T {
         
         // 1. Locate the json file
         
@@ -25,7 +25,7 @@ extension Bundle {
         let decoder = JSONDecoder()
         
         // 4. Create a property for the decoded data
-        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
         
